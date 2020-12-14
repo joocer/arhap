79c7ec67acb555104f2bdd54a8c983c8ef76e58d
Justin Joyce <justin.joyce@joocer.com>
2020-12-14 19:46:45 +0000

    initial checkin

--- xia/xia.py
+++ xia/xia.py
@@ -1,22 +1,26 @@
 import os
+import string
+from typing import Any, Union
 
-def random_string(length=64, characters=string.ascii_letters + string.digits):
+def random_string(
+        length:int = 64,
+        characters:str = string.ascii_letters + string.digits) -> str:
     result_str = ''.join(random_choice(characters) for i in range(length))
     return result_str
 
-def random_int():
+def random_int() -> int:
     """
     Select a random integer (64bit)
     """
     return bytes_to_int(os.urandom(8))
 
-def random_range(min, max):
+def random_range(min:int, max:int) -> int:
     """
     Select a random integer between two numbers
     """
     return (random_int() % ((max + 1) - min)) + min
 
-def bytes_to_int(bytes):
+def bytes_to_int(bytes: bytes) -> int:
     """
     Helper function, convert set of bytes to an integer
     """
@@ -25,7 +29,7 @@ def bytes_to_int(bytes):
         result = result * 256 + int(b)
     return result
 
-def random_choice(options):
+def random_choice(options: Union[list, str]) -> Any:
     """
     Select an item from a list of values
     """
